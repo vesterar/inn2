9faf3b650e0433ea97dfcae0606740effac45818
arve <arve.vesteraas@hotmail.com>
2025-01-15 21:26:17 +0100

    initial commit

---
+++ arbeidskrav2.1.py
@@ -0,0 +1,6 @@
+# Program som tar ditt fødselsår for å beregne din alder.
+
+born = int(input("Skriv inn hvilket år du er født: "))
+
+age = (2025 - born)
+print(f"Du er eller blir {age} år i 2025")
