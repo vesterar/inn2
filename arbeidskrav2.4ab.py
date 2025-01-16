972d8dc6e077b36c0ba44c33126d5392e8127d07
vesterar <151658065+vesterar@users.noreply.github.com>
2025-01-16 21:31:11 +0100

    Add files via upload

---
+++ arbeidskrav2.4ab.py
@@ -0,0 +1,23 @@
+data = {"Norge": ["Oslo", 0.634],
+        "England": ["London", 8.982],
+        "Frankrike": ["Paris", 2.161],
+        "Italia": ["Roma", 2.873]
+        }
+
+
+def hent_befolkning(land):
+    if land in data:
+        return data[land][1]
+
+    else:
+        return "landet finnes ikke i dictionaryen"
+
+
+def hent_hovedstad_(land):
+    if land in data:
+        return data[land][0]
+
+land = input("Skriv ett land: ")
+befolkning = hent_befolkning(land)
+hovedstad = hent_hovedstad_(land)
+print(f"{hovedstad} er hovedstaden i {land} og det er {befolkning} mill. innbyggere i {hovedstad}.")
